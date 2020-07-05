662b2ea09959760f9520f7dddc4f979662a2bc53
Антон Войтенко <avdev1987@gmail.com>
2020-07-05 15:23:45 +0500

    Сommit5

--- src/ru/progwards/java1/lessons/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/ReverseDigits.java
@@ -4,13 +4,13 @@ import java.sql.SQLOutput;
 
 public class ReverseDigits {
     public static int reverseDigits(int number) {
-      int a= (number%10);
-      int b= (number/10%10);
-      int c= (number/100);
+        int a = (number % 10);
+        int b = (number / 10 % 10);
+        int c = (number / 100);
         System.out.print(a);
         System.out.print(b);
         System.out.print(c);
-      return (a+b+c);
+        return (a + b + c);
 
     }
     public static void main(String[] args)
